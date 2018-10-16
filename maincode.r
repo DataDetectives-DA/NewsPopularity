51f2a90477e262e02e478b558154df9725ae8120
alka-si <33919005+alka-si@users.noreply.github.com>
2018-10-16 16:57:24 +0530

    feature reduction and basic linear model

--- maincode.r
+++ maincode.r
@@ -67,12 +67,51 @@ plot(prop_var, xlab = "Principal Component",ylab = "Proportion of Variance Expla
 plot(cumsum(prop_var), xlab = "Principal Component",ylab = "Cumulative Proportion of Variance Explained",type = "b")
 
 #-----------------------------------------MODELS -----------------------------------------------
-set.seed(20)
-train_data_index<-sample(1:nrow(News),0.8*(nrow(News)),replace=FALSE);
-test_data_index<-setdiff(1:nrow(News),train_data_index);
-train_data<-News[train_data_index,]
-test_data<-News[test_data_index,]
-model1<- lm(formula=shares~.,data=train_data)
-predmodel1<-predict(model1,test_data)
-errormodel1<-test_data$shares - predmodel1 
-rmse_usingmodel1<-sqrt(mean((errormodel1)^2)) 
+# Sampling the dataset into 75% : training data and 25% : test data:
+
+set.seed(400)
+
+NewsTrain <- sample(nrow(News),as.integer(nrow(News)*0.75))
+train.news = News[NewsTrain,]
+test.news = News[-NewsTrain,]
+
+# Now, we fit a model with all the variables; shares being the dependent variable and all other explanatory variables from the dataset as the predictors.
+
+fit_1 <- lm(shares ~ ., data = train.news)
+
+summary(fit_1)
+
+pred_1 = predict(fit_1, test.news)
+
+sqrt(mean((test.news$shares - pred_1)^2))
+
+# This model has a low R-square value, we can use a transformation on the model.
+
+# Now, I try using a log-transformation on our target variable to optimise our fit model:
+
+News$shares <- log(News$shares)
+
+NewsTrain <- sample(nrow(News),as.integer(nrow(News)*0.75))
+train.news = News[NewsTrain,]
+test.news = News[-NewsTrain,]
+
+# Now, I try fitting a model on the transformed target variable:
+
+fit_2 <- lm(shares ~ ., data = train.news)
+
+summary(fit_2)
+
+# This model gives a better R-square value than the previous model.
+
+# Now, I want to include only statistically significant variables in the model. So, I use the Stepwise regression step():
+
+fit_step_reg <- step(fit_2)
+
+summary(fit_step_reg)
+#this is giving us 41 relevant variables and we also see the pca was also giving us almost the same results.
+
+pred_2 = predict(fit_step_reg, test.news)
+
+sqrt(mean((test.news$shares - pred_2)^2))
+
+# We thus get an optimized model with R-square value of approximately 0.13 and Root mean square error of approximately 0.87. This model includes only the statiscal variables.
